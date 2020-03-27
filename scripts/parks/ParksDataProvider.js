d283083057dd9c2b03b10f1ee95652e98ea7ca84
Parker Hodge <iparkerhodge@gmail.com>
2020-03-27 11:50:17 -0500

    added a PARKS dropdown (not eatery dropdown) to the DOM

--- scripts/parks/ParksDataProvider.js
+++ scripts/parks/ParksDataProvider.js
@@ -7,8 +7,8 @@ import apiKeys from "../Settings.js"
 let parks = []
 
 export const useParks = () => {
-    const filteredParks = parks.filter( () => {
-        if (parks.designation.contains("National Park")) {
+    const filteredParks = parks.filter( (object) => {
+        if (object.designation.includes("National Park")) {
             return true
         }
         return false
@@ -19,13 +19,13 @@ export const useParks = () => {
 
 // fetching parks from API
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?api_key=${apiKeys.npsKey}`)
+    return fetch(`https://developer.nps.gov/api/v1/parks?limit=91&q=National%20Park&api_key=${apiKeys.npsKey}`)
     //taking what was recieved(promise) and turning it into javascript
         .then(response => response.json())
         //taking that javascript and storing it then putting it in parks
         .then(parsedParks => {
             parks = parsedParks.data
-        }).then(() => console.log(parks))
+        })
 }
 
 
