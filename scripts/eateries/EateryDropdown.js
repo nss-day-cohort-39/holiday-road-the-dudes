71678da52e98918e562849f89de6503e556255b5
Brant2001 <brant.pippin@gmail.com>
2020-03-27 13:45:49 -0500

    creted eateries dropdown and sorted them in aphabetical order by state

--- scripts/eateries/EateryDropdown.js
+++ scripts/eateries/EateryDropdown.js
@@ -5,9 +5,10 @@
 //Imports the Eatery data from the eatery data provider
 import { useEateries } from "./EateryDataProvider.js"
 
+const contentTarget = document.querySelector(".eateryDropdown")
 
 export const eateriesSelect = () => {
-
+const eateries = useEateries()
     const renderEateriesDropdown = (eateriesCollection) => {
         const eateries = useEateries()
         return `
@@ -15,12 +16,12 @@ export const eateriesSelect = () => {
             <option value="0">Places to Eat</option>   
             ${
                 eateriesCollection.map(singleEatery => {
-                    return `<option>${singleEatery.name}</option>`
+                    return `<option>${singleEatery.businessName} (${singleEatery.city}, ${singleEatery.state})</option>`
                 })
             }
         </select> `
         }
 
-    renderEateriesDropdown(eateries)
+    contentTarget.innerHTML = renderEateriesDropdown(eateries)
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ import { getEateries } from "./eateries/EateryDataProvider.js";
 import { getAttractions } from "./attractions/BizzareDataProvider.js";
 import { getWeather } from "./weather/WeatherDataProvider.js";
 import { parksSelect } from "./parks/ParksDropdown.js";
+import { eateriesSelect } from "./eateries/EateryDropdown.js";
 
 
 
 // getAttractions()
-// getEateries()
+getEateries().then(eateriesSelect)
 getParks().then(parksSelect)
 // getWeather()
