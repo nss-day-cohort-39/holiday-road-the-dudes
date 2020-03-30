73bb41b2755327214e0d35f02e20055cc3b97383
buckleyderekp <buckleyderekp@gmail.com>
2020-03-30 09:32:22 -0500

    copied info from api to our own json file to fetch faster for coding

--- scripts/parks/ParksDataProvider.js
+++ scripts/parks/ParksDataProvider.js
@@ -19,7 +19,8 @@ export const useParks = () => {
 
 // fetching parks from API
 export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?limit=91&q=National%20Park&api_key=${apiKeys.npsKey}`)
+    // return fetch(`https://developer.nps.gov/api/v1/parks?limit=91&q=National%20Park&api_key=${apiKeys.npsKey}`)
+    return fetch ('http://localhost:3000/parks')
     //taking what was recieved(promise) and turning it into javascript
         .then(response => response.json())
         //taking that javascript and storing it then putting it in parks
