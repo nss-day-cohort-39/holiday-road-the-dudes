cf235ad91d6bf26349f65f24cee93ae39650e177
buckleyderekp <buckleyderekp@gmail.com>
2020-03-30 11:25:42 -0500

    created content target

---
+++ scripts/itinerary/SaveItineraryPreview.js
@@ -0,0 +1,37 @@
+/*
+    This component holds the HTML representation for the save button for the itenerary and provides the functionality for it
+*/
+const contentTarget = document.querySelector('#saveItinerary') 
+
+ const eventHub = document.querySelector('.container')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  eventHub.addEventListener("click", e => {
+//     if(e.target.id ==="submit") {
+//         const entryDate = document.querySelector("#journalDate").value
+//         const entryConcepts = document.querySelector("#concepts").value
+//         const entryText = document.querySelector("#journalEntry").value
+//         const entryMood = document.querySelector("#mood").value
+
+//         const newEntry = {
+//             date: entryDate,
+//             concept: entryConcepts,
+//             entry: entryText,
+//             mood: entryMood
+//         }
+//         saveEntries(newEntry)
+//     }
+// })
